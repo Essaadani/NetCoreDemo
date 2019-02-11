61cc094e9ecd572c708c06450def720a685c0bbe
Khalid ESSAADANI <khalid_essaadani@hotmail.fr>
2019-02-12 00:50:43 +0100

    Add project files.

---
+++ NetCoreDemo/Computer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreDemo
+{
+    class Computer
+    {
+        //Properties
+        private string Brand;
+        public double price;
+        public DateTime ManufactureDate;
+        public string Color { get; set; }
+        //Auto Properties
+        public bool isNew { get; set; }
+
+        //Access Modifiers: private, public, internal, protected
+
+        //Full Properties
+        public string BrandProperty
+        {
+            get
+            {
+                return Brand.ToUpper();
+            }
+            set
+            {
+                Brand = value;
+            }
+        }
+
+
+    }
+}
